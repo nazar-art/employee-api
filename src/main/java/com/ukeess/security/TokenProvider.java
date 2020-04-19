98e93d07d059a0d2420a53ebc8e99898bbd1a64e
Nazar Lelyak <nazar.lelyak@gmail.com>
2020-04-19 22:53:57 +0300

    update expiration time for a token

--- src/main/java/com/ukeess/security/TokenProvider.java
+++ src/main/java/com/ukeess/security/TokenProvider.java
@@ -48,7 +48,7 @@ public class TokenProvider {
                 .setClaims(claims)
                 .setSubject(subject)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // 10 hours is expiration date
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24)) // 24 hours is expiration date
                 .signWith(SignatureAlgorithm.HS256, SECRET_KEY).compact();
     }
 
