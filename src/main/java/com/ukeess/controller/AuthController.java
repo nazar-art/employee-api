991c3e02c28c40862750b78d2951178a6006e791
Nazar Lelyak <nazar.lelyak@gmail.com>
2020-04-22 19:29:50 +0300

    small updates

--- src/main/java/com/ukeess/controller/AuthController.java
+++ src/main/java/com/ukeess/controller/AuthController.java
@@ -2,10 +2,10 @@ package com.ukeess.controller;
 
 import com.ukeess.exception.InvalidUserCredentialsException;
 import com.ukeess.model.constant.SecurityConstants;
+import com.ukeess.model.dto.AuthRequestDTO;
+import com.ukeess.model.dto.AuthResponseDTO;
 import com.ukeess.security.TokenProvider;
 import com.ukeess.security.UserDetailsServiceMock;
-import com.ukeess.security.model.AuthRequestDTO;
-import com.ukeess.security.model.AuthResponseDTO;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpHeaders;
