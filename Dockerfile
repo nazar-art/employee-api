b89309e1971938df37563434642c8ad9822491f0
Nazar Lelyak <nazar.lelyak@gmail.com>
2020-05-27 20:58:13 +0300

    add stub for docker configuration

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM openjdk:11-jre-slim
+
+ARG JAR_FILE=target/*.jar
+COPY ${JAR_FILE} employee-api.jar
+
+ENTRYPOINT ["java", "-jar", "/employee-api.jar"]
+
+
---
+++ mysql_init/schema.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS tblEmployees;
+DROP TABLE IF EXISTS tblDepartments;
+
+CREATE TABLE IF NOT EXISTS tblDepartments
+(
+    dpID   SERIAL,
+    dpName VARCHAR(10) NOT NULL,
+    PRIMARY KEY (dpID)
+);
+
+CREATE TABLE IF NOT EXISTS tblEmployees
+(
+    empID     SERIAL,
+    empName   VARCHAR(20)     NOT NULL,
+    empActive BIT(1)          NOT NULL,
+    emp_dpID  BIGINT UNSIGNED NOT NULL,
+    PRIMARY KEY (empID),
+    FOREIGN KEY (emp_dpID) REFERENCES tblDepartments (dpID)
+);
